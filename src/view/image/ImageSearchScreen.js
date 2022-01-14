d62d9c8f2c2da47b047e7059cf192b8c48e1aec3
Mukesh Maurya <mukesh.maurya@vedantu.com>
2022-01-14 19:34:15 +0530

    Image search need to fix search input placing.

--- src/view/image/ImageSearchScreen.js
+++ src/view/image/ImageSearchScreen.js
@@ -1,47 +1,48 @@
-import react, { useState, useEffect} from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, TextInput, View ,Image} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View, Image, ActivityIndicator, FlatList } from 'react-native'
 import CardView from '../custom/CardView';
+import axios from 'axios';
 
 const url = 'https://pixabay.com/api/?key=9140105-4c2e6a4eadeb7f0ace651bc11&image_type=photo'
 const ImageSearchScreen = () => {
     const [searchText, setSearchText] = useState('');
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
-    const doSearch = ()=>{
+    const doSearch = () => {
         axios.get(url)
-        .then((response)=>response.data)
-        .then((json)=>setData(json.hits))
-        .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
+            .then((response) => response.data)
+            .then((json) => setData(json.hits))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
     }
 
     useEffect(() => {
-       doSearch();
+        doSearch();
     }, [])
-
+    console.log("mk>>" + JSON.stringify(data))
     return (
         <SafeAreaView>
             <View style={styles.contentContainer}>
-                <View style={styles.searchContainer}>
-                <TextInput 
-                style={styles.searchInput}
-                placeholder='Search'
-                onChangeText={setSearchText}
-                />
-                <Button title='Search' onPress={doSearch}/>
-                </View>
+                <CardView style={styles.searchContainer}>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder='Search'
+                        onChangeText={setSearchText}
+                    />
+                    <Button title='Search' onPress={doSearch} />
+                </CardView>
             </View>
-            {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-              <CardView style={styles.cardSpacing}>
-                  <Image style={styles.image} source={{uri:item.imageURL}}/>
-            </CardView>
-          )}
-        />
-      )}
+            {isLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                        <CardView style={styles.cardSpacing}>
+                            <Image style={styles.image} source={{ uri: item.largeImageURL }} />
+                        </CardView>
+                    )}
+                />
+            )}
         </SafeAreaView>
     )
 }
@@ -50,29 +51,35 @@ export default ImageSearchScreen;
 
 const styles = StyleSheet.create({
     contentContainer: {
+        flexDirection: 'column',
         flex: 1,
         backgroundColor: '#fff',
-        padding:16,
     },
     searchContainer: {
-        flexDirection:'row'
+        flex: 1,
+        flexDirection: 'row',
     },
-    searchInput:{
-     padding:10,
-     backgroundColor:'#aeaeae',
-     borderRadius:8,
+    searchInput: {
+        padding: 10,
+        width: '70%',
+        height: 24,
+        backgroundColor: '#aeaeae',
+        borderRadius: 8,
     },
-    submitButton:{
+    submitButton: {
 
     },
     cardSpacing: {
-        margin:8
+        marginStart: 12,
+        marginEnd: 12,
+        marginTop: 8,
+        marginBottom: 8
     },
     image: {
-        width: '98%',
+        width: '100%',
         height: 200,
         borderRadius: 12
-      },
+    },
 
 
 })
